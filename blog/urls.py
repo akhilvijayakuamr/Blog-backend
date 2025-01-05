780305f8e79e19d104e6a231b1266229f5fbf62d
Akhil Vijayakumar <akhilvijayakumar333@gmail.com>
2025-01-05 22:47:31 +0530

    change urlpattes

--- blog/urls.py
+++ blog/urls.py
@@ -20,11 +20,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')), #-> Register my app urls
 ]
 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
